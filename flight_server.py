eedb84de279703c951f791ab49f018f9d18408f9
varoOP <46842159+varoOP@users.noreply.github.com>
2023-11-28 13:15:11 -0800

    feat(flight_server): modify and delete

--- flight_server.py
+++ flight_server.py
@@ -1,58 +1,66 @@
 import pyarrow.flight as flight
 import pyarrow as pa
-import pandas as pd
-
+import logging
 
 class FlightServer(flight.FlightServerBase):
+    _schema = pa.schema([
+        ('key', pa.string()), 
+        ('key_hash_val', pa.int64()), 
+        ('is_primary', pa.bool_()), 
+        ('is_secondary', pa.bool_())
+    ])
 
     def __init__(self, location, **kwargs):
-        super(FlightServer, self).__init__(location, **kwargs)
+        super().__init__(location, **kwargs)
         self._location = location
-        self._schema = {
-            'key': [], 'key_hash_val': [], 'is_primary': [], 'is_secondary': []}
-        initial_df = pd.DataFrame(self._schema)
-        self._data = pa.Table.from_pandas(initial_df)
+        self._data = pa.Table.from_schema(self._schema)
 
     def do_put(self, context, descriptor, reader, writer):
         description = descriptor.path[0].decode('utf-8')
 
-        if description == "insert":
-            data_table = reader.read_all()
-            data_to_insert = data_table.to_pandas()
+        try:
+            if description == "insert":
+                data_table = reader.read_all()
+                self._data = pa.concat_tables([self._data, data_table])
 
-            self._data = pa.Table.from_pandas(self._data.to_pandas()._append(data_to_insert, ignore_index=True))
+            elif description == "reset":
+                self._data = pa.Table.from_schema(self._schema)
 
-        if description == "reset":
-            self._data = pa.Table.from_pandas(self._data)
+            elif description == "modify":
+                request_data_table = reader.read_all()
+                request_data_df = request_data_table.to_pandas()
 
-        if description == "modify":
-            request_data_table = reader.read_all()
-            request_data_df = request_data_table.to_pandas()
+                modified_df = self._data.to_pandas()
+                for index, row in request_data_df.iterrows():
+                    key = row['key']
+                    is_primary = row['is_primary']
+                    is_secondary = row['is_secondary']
+                    modified_df.loc[modified_df['key'] == key, 'is_primary'] = is_primary
+                    modified_df.loc[modified_df['key'] == key, 'is_secondary'] = is_secondary
 
-            keys = request_data_df['keys'].tolist()
+                self._data = pa.Table.from_pandas(modified_df, preserve_index=False)
 
-            modified_df = self._data.to_pandas()
-            modified_df.loc[modified_df['key'].isin(keys), 'is_primary'] = 0
-            modified_df.loc[modified_df['key'].isin(keys), 'is_secondary'] = 1
+            elif description == "delete":
+                request_data_table = reader.read_all()
+                request_data_df = request_data_table.to_pandas()
 
-            self._data = pa.Table.from_pandas(modified_df)
+                keys_to_delete = request_data_df['key'].tolist()
+                modified_df = self._data.to_pandas()
+                modified_df = modified_df[~modified_df['key'].isin(keys_to_delete)]
 
-        if description == "delete":
-            request_data_table = reader.read_all()
-            request_data_df = request_data_table.to_pandas()
+                self._data = pa.Table.from_pandas(modified_df, preserve_index=False)
 
-            keys = request_data_df['keys'].tolist()
-
-            modified_df = self._data.to_pandas()
-            modified_df = modified_df[~modified_df['key'].isin(keys)]
-
-            self._data = pa.Table.from_pandas(modified_df)
+        except Exception as e:
+            logging.error(f"Error in do_put: {e}")
 
     def do_get(self, context, ticket):
-        return flight.RecordBatchStream(self._data)
-
+        try:
+            return flight.RecordBatchStream(self._data)
+        except Exception as e:
+            logging.error(f"Error in do_get: {e}")
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
     server = FlightServer(location="grpc://0.0.0.0:8815")
-    print("Starting the server on localhost:8815")
+    logging.info("Starting the server on localhost:8815")
     server.serve()
