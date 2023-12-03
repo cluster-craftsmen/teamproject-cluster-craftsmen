db8f5fe99ab0638202757851c3de6a861d6b72a5
varoOP <46842159+varoOP@users.noreply.github.com>
2023-12-03 11:57:14 -0800

    fix: BarGraph.tsx

--- frontend/src/BarGraph.tsx
+++ frontend/src/BarGraph.tsx
@@ -1,32 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Chart } from 'primereact/chart';
 import { useChartData } from './ChartDataContext';
 
-interface ServerData {
-  primary_data_count: number;
-  secondary_data_count: number;
-}
-
 const BarGraph: React.FC = () => {
-  const { setChartData } = useChartData();
-  const [serverData, setServerData] = useState<ServerData[]>([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch('/api/get_data');
-        if (!response.ok) {
-          throw new Error('Failed to fetch data');
-        }
-        const result: ServerData[] = await response.json();
-        setServerData(result);
-        setChartData(result.map((server) => server.primary_data_count));
-      } catch (error) {
-        console.error('Error fetching data');
-      }
-    };
-    fetchData();
-  }, [setChartData]);
+  const { serverData } = useChartData();
 
   const getLightTheme = () => {
     return {
@@ -65,36 +42,31 @@ const BarGraph: React.FC = () => {
   const { basicOptions } = getLightTheme();
 
   return (
-    <>
-      <div>
-        <div className="card">
-          {serverData.length > 0 && (
-            <div>
-              <div> {serverData[0].primary_data_count} </div>
-              <Chart
-                type="bar"
-                data={{
-                  labels: ['Server 1', 'Server 2', 'Server 3', 'Server 4'],
-                  datasets: [
-                    {
-                      label: 'Primary Data',
-                      backgroundColor: '#42A5F5',
-                      data: serverData.map((server) => server.primary_data_count),
-                    },
-                    {
-                      label: 'Replicated Data',
-                      backgroundColor: '#FFA726',
-                      data: serverData.map((server) => server.secondary_data_count),
-                    },
-                  ],
-                }}
-                options={basicOptions}
-              />
-            </div>
-          )}
-        </div>
+    <div>
+      <div className="card">
+        {serverData.length > 0 && (
+          <Chart
+            type="bar"
+            data={{
+              labels: ['Server 1', 'Server 2', 'Server 3', 'Server 4'], 
+              datasets: [
+                {
+                  label: 'Primary Data',
+                  backgroundColor: '#42A5F5',
+                  data: serverData.map(data => data.primary_data_count),
+                },
+                {
+                  label: 'Replicated Data',
+                  backgroundColor: '#FFA726',
+                  data: serverData.map(data => data.secondary_data_count),
+                },
+              ],
+            }}
+            options={basicOptions}
+          />
+        )}
       </div>
-    </>
+    </div>
   );
 };
 
